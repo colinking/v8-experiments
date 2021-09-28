f5da93ba86c51ff9b5e59a39e5be497fc91408ae
Colin King <me@colinking.co>
2021-09-28 18:49:59 -0400

    fork comment

--- proxy-crash/main.go
+++ proxy-crash/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 
+	// This example uses our fork for darwin_arm64 support, but the issue
+	// should surface using the upstream master, too:
+	// v8 "rogchap.com/v8go"
 	v8 "github.com/airplanedev/v8go"
 )
 
